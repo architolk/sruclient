7276aa32a7a4b56939ad9e8ecc09e463e798a4e5
architolk <msj@xs4all.nl>
2026-09-13 10:38:39 +0200

    Toevoeging generatie van het model

--- bwb2rdf.sh
+++ bwb2rdf.sh
@@ -1,6 +1,10 @@
 # Transform to RDF
 java -jar lib/saxon9.jar -s:data/BWBR0015703.xml -xsl:bwb2rdf.xsl -o:data/BWBR0015703.rdf
-# Convert XML/RDF to Turtle
+# Convert XML/RDF to Turtle and JSON-LD
 java -jar lib/rdf2rdf.jar -i data/BWBR0015703.rdf -o data/BWBR0015703.ttl
+java -jar lib/rdf2rdf.jar -i data/BWBR0015703.rdf -o data/BWBR0015703.json
 # Sanity check: convert back to text (markdown)
 java -jar lib/rdf2xml.jar data/BWBR0015703.rdf data/BWBR0015703.md rdf2md.xsl
+# Create SHACL fiel for data structure
+#java -jar lib/rdf2rdf.jar -i data/BWBR0015703.rdf -o data/model.ttl -c lib/rdf2sh.yaml
+java -jar lib/rdf2xml.jar data/model.ttl data/model.graphml lib/rdf2graphml.xsl add data/model-edited.graphml
